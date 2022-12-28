3245c8fc9d2ddc2343846d0711057c3d3bd6f6ef
gonzs <gonzalosisnero@gmail.com>
2022-12-28 15:00:56 -0300

    move `import` to the top

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,8 @@
+import utilStyles from "../../styles/utils.module.css";
 import Head from "next/head";
 import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
   return (
