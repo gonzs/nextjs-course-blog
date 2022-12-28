c88b989444ca79938b579eba951a70dd41c00412
gonzs <gonzalosisnero@gmail.com>
2022-12-27 22:48:37 -0300

    add layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -14,6 +14,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">← Back to Home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
