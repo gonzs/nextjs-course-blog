733912be426dff3c3300bf96db3a1f82a446221c
gonzs <gonzalosisnero@gmail.com>
2022-12-27 22:23:33 -0300

    add `Head`

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,18 @@
+import Head from "next/head";
 import Link from "next/link";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
 
       <h2>
-        <Link href="/">Back to Home</Link>
+        <Link href="/">← Back to Home</Link>
       </h2>
     </>
   );
