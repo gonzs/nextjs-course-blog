cb366a0c0c92fc752ac7902d56548ea32008a7a0
gonzs <gonzalosisnero@gmail.com>
2022-12-27 22:05:54 -0300

    add new page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+    </>
+  );
+}
